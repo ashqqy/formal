35987e75efb034a1f214924d2d2213324435cf13
artiom <artiom.shaff07@gmail.com>
2026-09-22 13:35:42 +0300

    build: apply coverage flags per target

--- cmake/coverage.cmake
+++ cmake/coverage.cmake
@@ -1,22 +1,22 @@
 option(FORMAL_COVERAGE "Build with coverage instrumentation" OFF)
 
+add_library(formal_coverage INTERFACE)
+
 if(NOT FORMAL_COVERAGE)
-  return()
+    return()
 endif()
 
-add_compile_options(--coverage)
-add_link_options(--coverage)
+target_compile_options(formal_coverage INTERFACE --coverage)
+target_link_options(formal_coverage INTERFACE --coverage)
 
 find_program(GCOVR gcovr REQUIRED)
 
-add_custom_target(coverage
+add_custom_target(
+    coverage
     COMMAND ${CMAKE_COMMAND} -E make_directory coverage
-    COMMAND ${GCOVR}
-            --root ${CMAKE_SOURCE_DIR}
-            --filter ${CMAKE_SOURCE_DIR}/src/
-            --html-details coverage/index.html
-            --print-summary
-            --delete
+    COMMAND
+        ${GCOVR} --root ${CMAKE_SOURCE_DIR} --filter ${CMAKE_SOURCE_DIR}/src/
+        --html-details coverage/index.html --print-summary --delete
     WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
     COMMENT "Generating coverage report"
 )
