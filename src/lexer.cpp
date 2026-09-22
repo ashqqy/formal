3bd779e43aba5d7b6e4762cf97861be07422e14c
artiom <artiom.shaff07@gmail.com>
2026-09-22 13:07:22 +0300

    refactor(lexer): move Lexer::next to lexer.cpp

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -1,1 +1,43 @@
 #include "lexer.hpp"
+
+#include <cctype>
+
+#include "error.hpp"
+#include "position.hpp"
+
+namespace regex {
+
+Token Lexer::next() {
+    while (!eof() && std::isspace(static_cast<unsigned char>(peek())) != 0) {
+        advance();
+    }
+
+    if (eof()) { return {TokenType::End, '\0', cursor_}; }
+
+    Position start = cursor_;
+    char c = advance();
+
+    switch (c) {
+        case '+':
+            return {TokenType::Plus, c, start};
+        case '.':
+            return {TokenType::Dot, c, start};
+        case '*':
+            return {TokenType::Star, c, start};
+        case '(':
+            return {TokenType::Lparen, c, start};
+        case ')':
+            return {TokenType::Rparen, c, start};
+        case '1':
+            return {TokenType::Epsilon, c, start};
+        case '\\': {
+            if (eof()) { throw SyntaxError("Dangling backslash", start); }
+            char d = advance();
+            return {TokenType::Letter, d, start};
+        }
+        default:
+            return {TokenType::Letter, c, start};
+    }
+}
+
+} // namespace regex
--- src/lexer.hpp
+++ src/lexer.hpp
@@ -1,11 +1,10 @@
 #pragma once
 
-#include <cctype>
 #include <cstdint>
 #include <string>
 #include <utility>
 
-#include "error.hpp"
+#include "error.hpp" // IWYU pragma: export
 #include "position.hpp"
 
 namespace regex {
@@ -42,39 +41,7 @@ class Lexer {
   public:
     explicit Lexer(std::string input) : input_(std::move(input)) {}
 
-    Token next() {
-        while (!eof() &&
-               std::isspace(static_cast<unsigned char>(peek())) != 0) {
-            advance();
-        }
-
-        if (eof()) { return {TokenType::End, '\0', cursor_}; }
-
-        Position start = cursor_;
-        char c = advance();
-
-        switch (c) {
-            case '+':
-                return {TokenType::Plus, c, start};
-            case '.':
-                return {TokenType::Dot, c, start};
-            case '*':
-                return {TokenType::Star, c, start};
-            case '(':
-                return {TokenType::Lparen, c, start};
-            case ')':
-                return {TokenType::Rparen, c, start};
-            case '1':
-                return {TokenType::Epsilon, c, start};
-            case '\\': {
-                if (eof()) { throw SyntaxError("Dangling backslash", start); }
-                char d = advance();
-                return {TokenType::Letter, d, start};
-            }
-            default:
-                return {TokenType::Letter, c, start};
-        }
-    }
+    Token next();
 
   private:
     [[nodiscard]] bool eof() const noexcept {
